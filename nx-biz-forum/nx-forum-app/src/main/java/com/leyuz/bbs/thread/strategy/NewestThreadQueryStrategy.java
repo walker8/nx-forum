dc8a08e9cd7a43674c2dedd6fb6116c59886c3aa
walkinglf <793061804@qq.com>
2025-06-15 13:48:04 +0800

    修改最新帖子时间范围

--- nx-biz-forum/nx-forum-app/src/main/java/com/leyuz/bbs/thread/strategy/NewestThreadQueryStrategy.java
+++ nx-biz-forum/nx-forum-app/src/main/java/com/leyuz/bbs/thread/strategy/NewestThreadQueryStrategy.java
@@ -10,27 +10,27 @@ import org.springframework.stereotype.Component;
 
 /**
  * 最新帖子查询策略
- * 
+ *
  * @author walker
  * @since 2025-03-01
  */
 @Component
 @Order(40) // 设置较高优先级
 public class NewestThreadQueryStrategy extends AbstractThreadQueryStrategy {
-    
+
     public NewestThreadQueryStrategy(IThreadService threadService, UserApplication userApplication) {
         super(threadService, userApplication);
     }
-    
+
     @Override
     public boolean supports(String forumName) {
         return "newest".equals(forumName);
     }
-    
+
     @Override
     public Page<ThreadPO> query(ThreadQuery threadQuery) {
         threadQuery.setOrderBy("create_time");
-        threadQuery.setDays(30);
+        threadQuery.setDays(180);
         return getThreadPOPage(0, threadQuery);
     }
 } 
