606737fe85a0f353def504e49d39013580bdf02a
walkinglf <793061804@qq.com>
2026-03-14 01:02:00 +0800

    修复 Logo 组件 SSR 水合不匹配问题

--- nx-forum-nuxt/components/common/logo.vue
+++ nx-forum-nuxt/components/common/logo.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex items-center justify-center p-4 h-[55px]">
-    <NuxtImg class="h-[55px]" src="/logo.png" alt="logo" />
+    <img class="h-[55px]" src="/logo.png" alt="logo" />
   </div>
 </template>
